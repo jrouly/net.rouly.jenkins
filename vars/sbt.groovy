84a8ac731a61bab481b08fc7f23ba805405b753b
Jean Michel Rouly <michel.rouly@gmail.com>
2020-03-30 19:45:02 -0400

    set sbt home directory

--- vars/sbt.groovy
+++ vars/sbt.groovy
@@ -1,7 +1,7 @@
 def call(String args) {
   sh """
-    sbt -Dsbt.global.base=.sbt \
-      -Dsbt.boot.directory=.sbt \
+    sbt -Dsbt.global.base=\$HOME/.sbt \
+      -Dsbt.boot.directory=\$HOME/.sbt \
       -Dsbt.ivy.home=.ivy2 \
       $args
   """
