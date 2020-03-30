5f0ccb83705be61c5b36fd77f08a316037145756
Jean Michel Rouly <michel.rouly@gmail.com>
2020-03-30 19:59:32 -0400

    stay in the workspace

--- vars/sbt.groovy
+++ vars/sbt.groovy
@@ -1,7 +1,9 @@
 def call(String args) {
   sh """
-    sbt -Dsbt.global.base=/.sbt \
-      -Dsbt.boot.directory=\$(mktemp -d) \
+    sbt \
+      -Dsbt.ci=true \
+      -Dsbt.global.base=/.sbt \
+      -Dsbt.boot.directory=.sbt \
       -Dsbt.ivy.home=.ivy2 \
       $args
   """
