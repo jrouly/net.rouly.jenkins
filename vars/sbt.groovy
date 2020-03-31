e2d1c37fd393c47eb66bd3c409cf9ec140931df8
Jean Michel Rouly <michel.rouly@gmail.com>
2020-03-30 20:58:29 -0400

    even the global sbt directory needs to be writeable

--- vars/sbt.groovy
+++ vars/sbt.groovy
@@ -1,8 +1,9 @@
 def call(String args) {
   sh """
+    cp -r /sbt .sbt
     sbt \
       -Dsbt.ci=true \
-      -Dsbt.global.base=/.sbt \
+      -Dsbt.global.base=.sbt \
       -Dsbt.boot.directory=.sbt \
       -Dsbt.ivy.home=.ivy2 \
       $args
